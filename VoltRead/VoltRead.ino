30d3d7d122bb5f99f77ac682b258335c1bc663ac
ajackows <atj236@nyu.edu>
2018-12-13 13:41:25 -0600

    Added main file
    Pretty much the only code for this
    - uses Arduino Analog read for ADC functions
    - uses Arduino Serial functions for exporting over UART
    
    The soon to be implemented command shell will run in the main loop in order to receive commands at a lower priority than the periodic measurements. 
    
    An interrupt is used to log measurements at a periodic rate. The interrupt will trigger for 30 ticks, and start logging for the last 10 ticks.
     The ADC will take 10 measurements and average the measurements and report that result.
    
    TODO: 
    
    Implement moving average to filter out errors in measurement
    
    Add time stamp to log (seconds)
    
    Implement command shell so datalog can be left and downloaded later

---
+++ VoltRead/VoltRead.ino
@@ -0,0 +1,141 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+#define VERBOSE
+//#define DEBUG
+
+#define VOLTREAD A0
+#define ADCMAXLVL 5.102
+#define ADCRES 1023
+
+
+//fucntion declarations
+int IntrSet();
+void ReadADC();
+
+//globals
+int numCnt;
+uint16_t tot = 0;
+
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  IntrSet();
+
+}
+
+void loop() 
+{
+
+
+
+  if( Serial.available()) 
+  {
+    char cmd = Serial.read();
+    //command case statement for logging upgrade
+    switch(cmd)
+    {
+      case 'a':
+        Serial.println("boop");
+        break;
+      case 'b': 
+        Serial.println("supdawg");
+        break;
+      default :
+        break;
+    }
+  }
+}
+
+void ReadADC(uint16_t movTot)
+{
+  //variable declarations
+  uint16_t adcRaw;
+  double volt;
+  double movAvg;
+  //read voltage from ADC
+  adcRaw = analogRead(VOLTREAD);
+
+  movAvg = movTot/10;
+  
+  //calculate volatage from ADC value using overvoltage correction
+  volt = ((double)adcRaw*ADCMAXLVL)/ADCRES; 
+  //print raw ADC value (for debug purposes)
+  //Serial.print(adcRaw);
+  //Serial.print("\t");
+
+  //print calculated voltage
+
+  
+  Serial.println(volt,3);  
+
+}
+
+
+uint16_t WindowAvg(uint16_t movgAvg)
+{
+  uint16_t adcRaw;
+  //read from ADC
+  adcRaw = analogRead(VOLTREAD);
+
+  
+  #ifdef DEBUG
+  Serial.println(movgAvg);
+  
+  Serial.print(adcRaw);
+  Serial.print("\t");
+  #endif
+  
+  if(movgAvg == 0)
+  {
+    return adcRaw;
+  }else{
+    return (adcRaw + movgAvg);
+  }
+  
+}
+
+int IntrSet()
+{
+   //set timer control register A for normal operation
+   TCCR1A = 0x00;
+   //set timer control register B to run clock with prescale 
+   TCCR1B =  (1 << CS12); // set 1024 prescaler
+
+   TIMSK1 = (1 << TOIE1); // enable timer1 overflow interrupt
+   TCNT1 = 0;
+  
+   sei(); // enable interrupts
+}
+
+
+
+ISR(TIMER1_OVF_vect)
+{
+  numCnt++;
+  #ifdef DEBUG
+  Serial.println(numCnt);
+  #endif
+
+
+  if(numCnt >= 20)
+  { 
+    tot = WindowAvg(tot);
+
+    #ifdef DEBUG
+    Serial.print("TOTAL: ");
+    Serial.println(tot);
+    #endif
+  }
+  if(numCnt >= 30)
+  {
+    numCnt = 0;
+    ReadADC(tot);
+    tot = 0;
+  }
+
+}
+
