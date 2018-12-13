155819553dbb4b557d56fb8e6b11fcc59b85706b
ajackows <atj236@nyu.edu>
2018-12-13 15:08:45 -0600

    Fixed Sliding Average, fixed timer settings
    Sliding average now works and produces the correct values
    
    Interrupt timer now triggers at 1 second as expected.
    
    Added timestamp to log time of voltage sample
    
    changed all counting to be triggered off of the time stamp

--- VoltRead/VoltRead.ino
+++ VoltRead/VoltRead.ino
@@ -3,7 +3,7 @@
 
 
 #define VERBOSE
-//#define DEBUG
+#define DEBUG
 
 #define VOLTREAD A0
 #define ADCMAXLVL 5.102
@@ -12,11 +12,13 @@
 
 //fucntion declarations
 int IntrSet();
-void ReadADC();
+void DisAvgpDat();
 
 //globals
-int numCnt;
+
 uint16_t tot = 0;
+uint32_t timeStamp = 0; //time stamp in seconds
+
 
 
 void setup() 
@@ -24,14 +26,12 @@ void setup()
   // put your setup code here, to run once:
   Serial.begin(9600);
   IntrSet();
-
 }
 
 void loop() 
 {
-
-
-
+  
+  
   if( Serial.available()) 
   {
     char cmd = Serial.read();
@@ -48,34 +48,32 @@ void loop()
         break;
     }
   }
+
+
+  
 }
 
-void ReadADC(uint16_t movTot)
+void DisAvgpDat(uint16_t movTot)
 {
   //variable declarations
   uint16_t adcRaw;
   double volt;
   double movAvg;
   //read voltage from ADC
-  adcRaw = analogRead(VOLTREAD);
 
-  movAvg = movTot/10;
+  movAvg = movTot/11;
   
   //calculate volatage from ADC value using overvoltage correction
-  volt = ((double)adcRaw*ADCMAXLVL)/ADCRES; 
-  //print raw ADC value (for debug purposes)
-  //Serial.print(adcRaw);
-  //Serial.print("\t");
+  volt = ((double)movAvg*ADCMAXLVL)/ADCRES; 
 
   //print calculated voltage
-
-  
+  Serial.print("Time(s): \t");
+  Serial.print (timeStamp);
+  Serial.print("\t Voltage(v): \t");
   Serial.println(volt,3);  
-
 }
 
-
-uint16_t WindowAvg(uint16_t movgAvg)
+uint16_t WindowTot(uint16_t movgTot)
 {
   uint16_t adcRaw;
   //read from ADC
@@ -83,17 +81,19 @@ uint16_t WindowAvg(uint16_t movgAvg)
 
   
   #ifdef DEBUG
-  Serial.println(movgAvg);
-  
+
+  Serial.print("raw Value:");
   Serial.print(adcRaw);
   Serial.print("\t");
   #endif
-  
-  if(movgAvg == 0)
+
+  //if this is the first value of the moving total replace the moving total with measurement else add to moving total
+  if(movgTot == 0)
   {
     return adcRaw;
-  }else{
-    return (adcRaw + movgAvg);
+  }else
+  {
+    return (adcRaw + movgTot);
   }
   
 }
@@ -103,11 +103,11 @@ int IntrSet()
    //set timer control register A for normal operation
    TCCR1A = 0x00;
    //set timer control register B to run clock with prescale 
-   TCCR1B =  (1 << CS12); // set 1024 prescaler
+   TCCR1B = (1<<CS10) | (1 << CS12); // set 256 prescaler
 
    TIMSK1 = (1 << TOIE1); // enable timer1 overflow interrupt
-   TCNT1 = 0;
-  
+   TCNT1 = 49911;
+      
    sei(); // enable interrupts
 }
 
@@ -115,27 +115,29 @@ int IntrSet()
 
 ISR(TIMER1_OVF_vect)
 {
-  numCnt++;
+  timeStamp++;
+  
   #ifdef DEBUG
-  Serial.println(numCnt);
+  Serial.println(timeStamp %30);
   #endif
-
-
-  if(numCnt >= 20)
+  //reset timer counter value so it counts from this number (used to ensure 1sec interrupts)
+  TCNT1 = 49911;
+  
+  if((timeStamp%30) >= 20)
   { 
-    tot = WindowAvg(tot);
+    //calculate total for moving average
+    tot = WindowTot(tot);
 
     #ifdef DEBUG
     Serial.print("TOTAL: ");
     Serial.println(tot);
     #endif
   }
-  if(numCnt >= 30)
+  if((timeStamp %30 == 0)&& timeStamp > 0)
   {
-    numCnt = 0;
-    ReadADC(tot);
-    tot = 0;
+    //display data to port
+    DisAvgpDat(tot);
+    tot = 0; // reset moving total
   }
 
 }
-
