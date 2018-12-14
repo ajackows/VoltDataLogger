a5959b5a022928905a35fe1a679d66df16f8a58a
ajackows <atj236@nyu.edu>
2018-12-14 12:16:39 -0600

    Updated Interrupt
    Updated the interrupt sequence so all ADC processing is done outside the interrupt to avoid (unlikely) concurrency issues. 
    While this was not necessary it was done for good practice.
    
    changed all timing to be based off of the main counter as to limit global variables, this does come at the price of performing more operations. Not sure which is more ideal but for this scenario it is irrelevant.

--- VoltRead/VoltRead.ino
+++ VoltRead/VoltRead.ino
@@ -3,10 +3,10 @@
 
 
 #define VERBOSE
-#define DEBUG
+// #define DEBUG
 
 #define VOLTREAD A0
-#define ADCMAXLVL 5.102
+#define ADCMAXLVL 5.102 //needs to be measured from 5v line of arduino
 #define ADCRES 1023
 
 
@@ -18,7 +18,9 @@ void DisAvgpDat();
 
 uint16_t tot = 0;
 uint32_t timeStamp = 0; //time stamp in seconds
+uint8_t itrFlg = 0;
 
+uint8_t stat= 0x0;
 
 
 void setup() 
@@ -31,15 +33,23 @@ void setup()
 void loop() 
 {
   
-  
   if( Serial.available()) 
   {
+
     char cmd = Serial.read();
+
     //command case statement for logging upgrade
     switch(cmd)
     {
+
+      //commands to implement:
+      // - store data log
+      // - print data log and clear
+      // - switch to verbose mode (display measurements)
       case 'a':
         Serial.println("boop");
+        
+        
         break;
       case 'b': 
         Serial.println("supdawg");
@@ -49,8 +59,26 @@ void loop()
     }
   }
 
-
+  if(itrFlg == 1)
+  {
+    if((timeStamp%30) >= 20)
+    { 
+      //calculate total for moving average
+      tot = WindowTot(tot);
   
+      #ifdef DEBUG
+      Serial.print("TOTAL: ");
+      Serial.println(tot);
+      #endif
+    }
+    if((timeStamp %30 == 0)&& timeStamp > 0)
+    {
+      //display data to port
+      DisAvgpDat(tot);
+      tot = 0; // reset moving total
+    }
+    itrFlg = 0;
+  }
 }
 
 void DisAvgpDat(uint16_t movTot)
@@ -61,7 +89,7 @@ void DisAvgpDat(uint16_t movTot)
   double movAvg;
   //read voltage from ADC
 
-  movAvg = movTot/11;
+  movAvg = movTot/10;
   
   //calculate volatage from ADC value using overvoltage correction
   volt = ((double)movAvg*ADCMAXLVL)/ADCRES; 
@@ -76,12 +104,12 @@ void DisAvgpDat(uint16_t movTot)
 uint16_t WindowTot(uint16_t movgTot)
 {
   uint16_t adcRaw;
+  
   //read from ADC
   adcRaw = analogRead(VOLTREAD);
 
   
   #ifdef DEBUG
-
   Serial.print("raw Value:");
   Serial.print(adcRaw);
   Serial.print("\t");
@@ -98,6 +126,7 @@ uint16_t WindowTot(uint16_t movgTot)
   
 }
 
+
 int IntrSet()
 {
    //set timer control register A for normal operation
@@ -107,37 +136,24 @@ int IntrSet()
 
    TIMSK1 = (1 << TOIE1); // enable timer1 overflow interrupt
    TCNT1 = 49911;
-      
+        
    sei(); // enable interrupts
 }
 
 
-
 ISR(TIMER1_OVF_vect)
 {
+  //increment time stamp (1 sec)
   timeStamp++;
+
+  //set interrupt flag for ADC operations
+  itrFlg = 1;
   
   #ifdef DEBUG
   Serial.println(timeStamp %30);
   #endif
+  
   //reset timer counter value so it counts from this number (used to ensure 1sec interrupts)
   TCNT1 = 49911;
   
-  if((timeStamp%30) >= 20)
-  { 
-    //calculate total for moving average
-    tot = WindowTot(tot);
-
-    #ifdef DEBUG
-    Serial.print("TOTAL: ");
-    Serial.println(tot);
-    #endif
-  }
-  if((timeStamp %30 == 0)&& timeStamp > 0)
-  {
-    //display data to port
-    DisAvgpDat(tot);
-    tot = 0; // reset moving total
-  }
-
 }
